fd821a3b3b5d017b54f2565c468f5f7cc354529c
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-07 12:51:42 -0500

    Creación código en la clase principal para dar solución al ejercicio de composición utilizando las clases Casa y Habitacion antes creadas.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import com.company.agregacion.Estudiante;
 import com.company.agregacion.Universidad;
+import com.company.composicion.Casa;
+import com.company.composicion.Habitacion;
 
 public class Main {
 
@@ -29,5 +31,27 @@ public class Main {
             System.out.print("\nEstudiante " + i + ": ");
             universidad1.getEstudiantes()[i].mostrarInformacion();
         }
+
+        // Ejecución ejercicio composición.
+
+        //Creando instancia de la clase Casa
+        Casa casa = new Casa();
+
+        //Creando instancias de la clase Habitacion
+        Habitacion habitacion1 = new Habitacion(1, 10);
+        Habitacion habitacion2 = new Habitacion(2, 5.3);
+
+        //Agregando habitaciones a la casa
+        casa.agregarHabitacion(habitacion1);
+        casa.agregarHabitacion(habitacion2);
+
+        //Mostrando información de la casa
+        casa.mostrarInformacionCasa();
+
+        //Llamando método destruirCasa
+        casa.destruirCasa();
+
+        //Mostrando información de la casa
+        casa.mostrarInformacionCasa();
     }
 }
