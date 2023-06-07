56921e775171f6026445b2ee2d6db6edfc1d860d
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-06-07 11:17:16 -0500

    Creación código en la clase principal para dar solución al ejercicio de agregación utilizando las clases Universidad y Estudiante antes creadas.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import com.company.agregacion.Estudiante;
+import com.company.agregacion.Universidad;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Ejecución ejercicio agregación.
+
+        //Creando instancia de la clase Universidad
+        Universidad universidad1 = new Universidad("Universidad XYZ");
+
+        //Creando instancias de la clase Estudiante
+        Estudiante estudiante1 = new Estudiante("Antonia", 22, 1);
+        Estudiante estudiante2 = new Estudiante("Joaquin", 32, 2);
+
+        //Agregando estudiantes a la universidad
+        universidad1.agregarEstudiante(estudiante1);
+        universidad1.agregarEstudiante(estudiante2);
+
+        //imprimiendo nombre universidad
+        System.out.println("El nombre de la universidad es: " +
+                universidad1.getNombre() + ".");
+
+        //Recorriendo el vector de estudiantes y mostrando la información de
+        // cada uno
+        for (int i = 0; i < universidad1.getCantidadDeEstudiantes(); i++) {
+            System.out.print("\nEstudiante " + i + ": ");
+            universidad1.getEstudiantes()[i].mostrarInformacion();
+        }
     }
 }
